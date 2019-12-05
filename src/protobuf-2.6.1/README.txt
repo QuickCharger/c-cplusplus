ac4b677a662016ec07a0ad2e63c5e8c07e45877c
qwer <qwer@google.com>
2019-12-05 16:18:25 +0800

    protobuf 2.6.1

--- src/protobuf-2.6.1/README.txt
+++
@@ -1,4 +0,0 @@
-下载自
-	https://github.com/protocolbuffers/protobuf/releases/download/v2.6.1/protobuf-2.6.1.zip
-
-未做任何添加修改
---
+++ src/protobuf-2.6.1/build_protobuf_2.6.1.bat
@@ -0,0 +1,8 @@
+rem wget https://github.com/protocolbuffers/protobuf/releases/download/v2.6.1/protobuf-2.6.1.zip
+rem unzip protobuf-2.6.1.zip
+cd protobuf-2.6.1
+
+rem open .sln
+rem add define _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS in libprotobuf project
+
+pause
