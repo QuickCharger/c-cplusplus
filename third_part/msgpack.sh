24abfb3bbe0fbd52a8f8dc294c94388e0481d295
bdgboss <bdgboss12@gmail.com>
2021-12-10 18:15:08 +0800

    third_part

---
+++ third_part/msgpack.sh
@@ -0,0 +1,14 @@
+wget https://github.com/msgpack/msgpack-c/releases/download/cpp-4.0.3/msgpack-cxx-4.0.3.tar.gz
+tar -zxf msgpack-cxx-4.0.3.tar.gz
+
+pushd msgpack-cxx-4.0.3/
+mkdir tmp
+pushd tmp
+cmake -D CMAKE_INSTALL_PREFIX=`pwd`/../../msgpack ..
+make
+make install
+popd
+popd
+
+rm msgpack-cxx-4.0.3.tar.gz
+rm -rf msgpack-cxx-4.0.3/
