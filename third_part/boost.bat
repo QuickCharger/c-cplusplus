c65444acdfc2713d78a82838134632ef81a04684
bdgboss <bdgboss12@gmail.com>
2021-12-11 18:17:07 +0800

    third_part

--- third_part/boost.bat
+++ third_part/boost.bat
@@ -1,8 +1,10 @@
 ﻿rem https://www.boost.org/doc/libs/1_78_0/more/getting_started/unix-variants.html
 rem 需要手动执行下面的命令
 
-wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.zip
-unzip boost_1_78_0.zip > null
-cd boost_1_78_0
-bootstrap.bat
-b2.exe install --prefix=`pwd`/../boost
+rem wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.zip
+rem unzip boost_1_78_0.zip > null
+rem cd boost_1_78_0
+rem bootstrap.bat
+rem b2.exe install --prefix=`pwd`/../boost
+
+pause
--- third_part/curl.bat
+++ third_part/curl.bat
@@ -1,12 +1,13 @@
-rem wget https://github.com/curl/curl/releases/download/curl-7_80_0/curl-7.80.0.tar.gz
-rem tar -zxf curl-7.80.0.tar.gz
+﻿rem 需要手动执行下面的命令
 
-cd curl-7.80.0
-mkdir sln
-cd sln
-cmake ..
-cmake --build . --target install --clean-first --config Release
-pause
+wget https://github.com/curl/curl/releases/download/curl-7_80_0/curl-7.80.0.tar.gz
+tar -zxf curl-7.80.0.tar.gz
+cd curl-7.80.0\winbuild
+CALL "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+nmake /f makefile.vc mode=static ENABLE_IDN=no debug=no
 cd ../..
-rm -rf curl-7.80.0.tar.gz
-rm -rf curl-7.80.0/
+mkdir curl
+cp -r curl-7.80.0/builds/libcurl-vc-x86-release-static-ipv6-sspi-schannel/* curl/
+rm -rf curl-7.80.0
+
+pause
