1c32626466bf1aa1f701beec4cc3d23af07d39ee
bdgboss <bdgboss12@gmail.com>
2021-12-14 13:34:48 +0800

    third_part

--- third_part/boost.bat
+++ third_part/boost.bat
@@ -1,7 +1,7 @@
 ﻿rem https://www.boost.org/doc/libs/1_78_0/more/getting_started/unix-variants.html
 
-wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.zip
-unzip boost_1_78_0.zip > null
+wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
+tar -zxf boost_1_78_0.tar.gz
 cd boost_1_78_0
 ./bootstrap.bat
 ./b2.exe install --prefix=../boost
