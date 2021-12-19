986c54f76004ea15eeb67a3f58ecef36cfa48abb
bdg <bdgboss@126.com>
2021-12-19 23:08:14 +0800

    third_part

--- third_part/boost.sh
+++ third_part/boost.sh
@@ -2,11 +2,11 @@
 
 wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
 tar -zxf boost_1_78_0.tar.gz
-rm boost_1_78_0.tar.gz
 
 pushd boost_1_78_0
 ./bootstrap.sh --prefix=`pwd`/../boost
 ./b2 install
 popd
 
+rm boost_1_78_0.tar.gz
 rm -rf boost_1_78_0
