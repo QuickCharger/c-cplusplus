84869fe722fea93382397025b06d92d60834d21a
bdg <bdgboss@126.com>
2021-12-20 03:56:40 +0800

    third_path

--- third_part/openssl.sh
+++ third_part/openssl.sh
@@ -1,6 +1,5 @@
 wget https://github.com/openssl/openssl/archive/refs/tags/openssl-3.0.0.tar.gz
 tar -xzf openssl-3.0.0.tar.gz
-rm openssl-3.0.0.tar.gz.tar.gz
 
 pushd openssl-openssl-3.0.0/
 ./config --prefix=`pwd`/../openssl
@@ -8,4 +7,5 @@ make
 make install
 popd
 
+rm -rf openssl-3.0.0.tar.gz
 rm -rf openssl-openssl-3.0.0/
