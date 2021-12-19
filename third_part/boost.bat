c2a76a0eff4f92460d8122aea2e6a26250ff55ff
bdgboss <bdgboss12@gmail.com>
2021-12-19 18:59:22 +0800

    third_part

--- third_part/boost.bat
+++ third_part/boost.bat
@@ -3,7 +3,7 @@
 wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
 tar -zxf boost_1_78_0.tar.gz
 cd boost_1_78_0
-./bootstrap.bat
-./b2.exe install --prefix=../boost
+call bootstrap.bat
+call b2.exe install --prefix=../boost
 
 pause
