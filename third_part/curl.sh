19f96493db2fbbd767c76b80702d3baf57447d42
bdg <bdgboss@126.com>
2021-12-19 23:07:08 +0800

    third_part

--- third_part/curl.sh
+++ third_part/curl.sh
@@ -1,13 +1,10 @@
 wget https://github.com/curl/curl/releases/download/curl-7_80_0/curl-7.80.0.tar.gz
 tar -zxf curl-7.80.0.tar.gz
 
-pushd curl-7.80.0
-mkdir tmp
-pushd tmp
+mkdir curl-7.80.0/sln
+pushd curl-7.80.0/sln
 cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../../curl ..
-make
-make install
-popd
+cmake --build . --target install --clean-first --config Release
 popd
 
 rm -rf curl-7.80.0.tar.gz
