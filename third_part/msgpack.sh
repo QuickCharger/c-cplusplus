adb99dc3ce0da57e16623ffa67f3b51cf4d4af0d
bdg <bdgboss@126.com>
2021-12-19 22:07:39 +0800

    third_path

--- third_part/msgpack.sh
+++ third_part/msgpack.sh
@@ -1,9 +1,13 @@
+# make sure you have done boost.sh first
+
 wget https://github.com/msgpack/msgpack-c/releases/download/cpp-4.0.3/msgpack-cxx-4.0.3.tar.gz
 tar -zxf msgpack-cxx-4.0.3.tar.gz
 
+export PATH=`pwd`/boost:$PATH
+
 pushd msgpack-cxx-4.0.3/
-mkdir tmp
-pushd tmp
+mkdir sln
+pushd sln
 cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../../msgpack ..
 make
 make install
