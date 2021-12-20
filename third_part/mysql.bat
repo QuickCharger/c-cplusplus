c5b15a5588bdb70a4654e3834a9df9f13136c5f5
bdgboss <bdgboss12@gmail.com>
2021-12-20 14:56:41 +0800

    third_part

---
+++ third_part/mysql.bat
@@ -0,0 +1,2 @@
+wget https://cdn.mysql.com/archives/mysql-8.0/mysql-8.0.26-winx64.zip
+unzip mysql-8.0.26-winx64.zip
---
+++ third_part/mysql.sh
@@ -0,0 +1,4 @@
+wget https://cdn.mysql.com/archives/mysql-8.0/mysql-8.0.26-linux-glibc2.17-x86_64-minimal-rebuild.tar.xz
+xz -kd mysql-8.0.26-linux-glibc2.17-x86_64-minimal-rebuild.tar.xz
+tar -xvf mysql-8.0.26-linux-glibc2.17-x86_64-minimal-rebuild.tar
+rm mysql-8.0.26-linux-glibc2.17-x86_64-minimal-rebuild.tar
